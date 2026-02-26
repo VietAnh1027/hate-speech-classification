4cbb727461c53b03ab3e2316ce7813317f5e6b88
VietAnh1027 <nvietanh1027@gmail.com>
2026-02-26 10:32:38 +0700

    Initial commit: Complete project

---
+++ app/app.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from huggingface_hub import snapshot_download
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from pyvi import ViTokenizer
+import os
+from pydantic import BaseModel
+import asyncio
+
+app = FastAPI()
+
+model_path = os.path.dirname(os.path.abspath(__file__)) + "/../hate-speech-model"
+def checking_download(model_path):
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+        print("Đã tạo thư mục model rỗng")
+
+    if not os.listdir(model_path):
+        print("Thư mục model rỗng, bắt đầu tải về ...")
+        snapshot_download(
+            repo_id="VietAnh-1027/hate-speech-model",
+            repo_type="model",
+            local_dir=model_path
+        )
+    else: print("Thư mục model đã tồn tại")
+
+checking_download(model_path)
+
+classes = ["noram", "rude", "toxic"]
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=3)
+model.to(device)
+model.eval()
+
+def classify_comment(text):
+    text = ViTokenizer.tokenize(text)
+    with torch.no_grad():
+        inputs = tokenizer(text, padding="max_length", max_length=100, truncation=True, return_tensors="pt").to(device)
+        outputs = model(**inputs)
+        probabilites = F.softmax(outputs.logits, dim=1)
+        confident, cls_id = torch.max(probabilites, dim=1)
+    return round(confident.item()*100, 2), cls_id
+
+class Comment(BaseModel):
+    user: str
+    cmt: str
+
+@app.post("/predict")
+async def predict(request: Comment):
+    conf_score, cls_id = await asyncio.to_thread(classify_comment, request.cmt)
+    return {
+        "user": request.user,
+        "comment": request.cmt,
+        "type": classes[cls_id],
+        "confident": f"{conf_score:.2f}%"
+    }
